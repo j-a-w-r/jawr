5146336026aaa1b68390d22d6ea46c19a920c752
Ibrahim Chaehoi <ichaehoi@gmail.com>
2009-07-13 20:44:45 +0000

    Add Facelet tags for image resource. Update documentation
    git-svn-id: https://svn.java.net/svn/jawr~svn/branches/3.0_full@377 4db09855-0bbe-6bc9-8408-d5225a1fabd9

--- core/src/main/java/net/jawr/web/taglib/ImgHtmlTag.java
+++ core/src/main/java/net/jawr/web/taglib/ImgHtmlTag.java
@@ -22,182 +22,183 @@ import javax.servlet.jsp.JspException;
  * This class defines the image tag.
  * 
  * This implementation is based on the Struts image tag.
- *  
+ * 
  * @author Ibrahim Chaehoi
  */
 public class ImgHtmlTag extends AbstractImageTag {
-	
-		// -------------------------------------------------------------
-	
-		/**
-	     * The image height.
-	     */
-	    protected String height = null;
 
-	    /**
-	     * The horizontal spacing around the image.
-	     */
-	    protected String hspace = null;
+	// -------------------------------------------------------------
 
-	    /**
-	     * Server-side image map declaration.
-	     */
-	    protected String ismap = null;
+	/**
+	 * The serial version UID
+	 */
+	private static final long serialVersionUID = -6048102958207543073L;
 
-	    /**
-	     * Client-side image map declaration.
-	     */
-	    protected String usemap = null;
+	/**
+	 * The image height.
+	 */
+	protected String height = null;
 
-	    /**
-	     * The vertical spacing around the image.
-	     */
-	    protected String vspace = null;
+	/**
+	 * The horizontal spacing around the image.
+	 */
+	protected String hspace = null;
 
-	    /**
-	     * The image width.
-	     */
-	    protected String width = null;
-	    
-	    // ----------------------------------------------------- Constructor
-	    
-	    public ImgHtmlTag() {
-	        
-	    }
+	/**
+	 * Server-side image map declaration.
+	 */
+	protected String ismap = null;
 
-	    public String getHeight() {
-	        return (this.height);
-	    }
+	/**
+	 * Client-side image map declaration.
+	 */
+	protected String usemap = null;
 
-	    public void setHeight(String height) {
-	        this.height = height;
-	    }
+	/**
+	 * The vertical spacing around the image.
+	 */
+	protected String vspace = null;
 
-	    public String getHspace() {
-	        return (this.hspace);
-	    }
+	/**
+	 * The image width.
+	 */
+	protected String width = null;
 
-	    public void setHspace(String hspace) {
-	        this.hspace = hspace;
-	    }
+	// ----------------------------------------------------- Constructor
 
-	   public String getIsmap() {
-	        return (this.ismap);
-	    }
+	public ImgHtmlTag() {
 
-	    public void setIsmap(String ismap) {
-	        this.ismap = ismap;
-	    }
-
-	    public String getUsemap() {
-	        return (this.usemap);
-	    }
-
-	    public void setUsemap(String usemap) {
-	        this.usemap = usemap;
-	    }
-
-	    public String getVspace() {
-	        return (this.vspace);
-	    }
-
-	    public void setVspace(String vspace) {
-	        this.vspace = vspace;
-	    }
-
-	    public String getWidth() {
-	        return (this.width);
-	    }
-
-	    public void setWidth(String width) {
-	        this.width = width;
-	    }
-
-	    // --------------------------------------------------------- Public Methods
-
-	   /**
-	     * Render the beginning of the IMG tag.
-	     *
-	     * @throws JspException if a JSP exception has occurred
-	     */
-	    public int doStartTag() throws JspException {
-	        
-	    	return super.doStartTag();
-	    }
-
-	    /**
-	     * Render the IMG tag.
-	     *
-	     * @throws JspException if a JSP exception has occurred
-	     */
-	    public int doEndTag() throws JspException {
-	        // Generate the name definition or image element
-	        HttpServletResponse response =
-	            (HttpServletResponse) pageContext.getResponse();
-	        StringBuffer results = new StringBuffer("<img");
-	        
-	        prepareImageUrl(response, results);
-	        
-	        prepareAttribute(results, "name", getName());
-	        prepareAttribute(results, "height", getHeight());
-	        prepareAttribute(results, "width", getWidth());
-	        prepareAttribute(results, "align", getAlign());
-	        prepareAttribute(results, "border", getBorder());
-	        prepareAttribute(results, "hspace", getHspace());
-	        prepareAttribute(results, "vspace", getVspace());
-	        prepareAttribute(results, "ismap", getIsmap());
-	        prepareAttribute(results, "usemap", getUsemap());
-	        results.append(prepareStyles());
-	        results.append(prepareEventHandlers());
-	        results.append(" />");
-
-	        try {
-				pageContext.getOut().write(results.toString());
-			} catch (IOException e) {
-				throw new JspException(e);
-			}
-
-	        return (EVAL_PAGE);
-	    }
-
-		/**
-	     * Release any acquired resources.
-	     */
-	    public void release() {
-	        super.release();
-
-	        border = null;
-	        height = null;
-	        hspace = null;
-	        name = null;
-	        ismap = null;
-	        src = null;
-	        usemap = null;
-	        vspace = null;
-	        width = null;
-	    }
-
-	    // ------------------------------------------------------ Protected Methods
-
-	    /**
-	     * Return the specified src URL, modified as necessary with optional
-	     * request parameters.
-	     *
-	     * @param url The URL to be modified (or null if this url will not be
-	     *            used)
-	     * @throws JspException if an error occurs preparing the URL
-	     */
-	    protected String url(String url)
-	        throws JspException {
-	        if (url == null) {
-	            return (url);
-	        }
-
-	        // Start with an unadorned URL as specified
-	        StringBuffer src = new StringBuffer(url);
-
-
-	        // Return the final result
-	        return (src.toString());
-	    }
 	}
+
+	public String getHeight() {
+		return (this.height);
+	}
+
+	public void setHeight(String height) {
+		this.height = height;
+	}
+
+	public String getHspace() {
+		return (this.hspace);
+	}
+
+	public void setHspace(String hspace) {
+		this.hspace = hspace;
+	}
+
+	public String getIsmap() {
+		return (this.ismap);
+	}
+
+	public void setIsmap(String ismap) {
+		this.ismap = ismap;
+	}
+
+	public String getUsemap() {
+		return (this.usemap);
+	}
+
+	public void setUsemap(String usemap) {
+		this.usemap = usemap;
+	}
+
+	public String getVspace() {
+		return (this.vspace);
+	}
+
+	public void setVspace(String vspace) {
+		this.vspace = vspace;
+	}
+
+	public String getWidth() {
+		return (this.width);
+	}
+
+	public void setWidth(String width) {
+		this.width = width;
+	}
+
+	// --------------------------------------------------------- Public Methods
+
+	/**
+	 * Render the beginning of the IMG tag.
+	 * 
+	 * @throws JspException if a JSP exception has occurred
+	 */
+	public int doStartTag() throws JspException {
+
+		return super.doStartTag();
+	}
+
+	/**
+	 * Render the IMG tag.
+	 * 
+	 * @throws JspException if a JSP exception has occurred
+	 */
+	public int doEndTag() throws JspException {
+		// Generate the name definition or image element
+		HttpServletResponse response = (HttpServletResponse) pageContext
+				.getResponse();
+		StringBuffer results = new StringBuffer("<img");
+
+		prepareImageUrl(response, results);
+
+		prepareAttribute(results, "name", getName());
+		prepareAttribute(results, "height", getHeight());
+		prepareAttribute(results, "width", getWidth());
+		prepareAttribute(results, "align", getAlign());
+		prepareAttribute(results, "border", getBorder());
+		prepareAttribute(results, "hspace", getHspace());
+		prepareAttribute(results, "vspace", getVspace());
+		prepareAttribute(results, "ismap", getIsmap());
+		prepareAttribute(results, "usemap", getUsemap());
+		results.append(prepareStyles());
+		results.append(prepareEventHandlers());
+		results.append(" />");
+
+		try {
+			pageContext.getOut().write(results.toString());
+		} catch (IOException e) {
+			throw new JspException(e);
+		}
+
+		return (EVAL_PAGE);
+	}
+
+	/**
+	 * Release any acquired resources.
+	 */
+	public void release() {
+		super.release();
+
+		border = null;
+		height = null;
+		hspace = null;
+		name = null;
+		ismap = null;
+		src = null;
+		usemap = null;
+		vspace = null;
+		width = null;
+	}
+
+	// ------------------------------------------------------ Protected Methods
+
+	/**
+	 * Return the specified src URL, modified as necessary with optional request parameters.
+	 * 
+	 * @param url The URL to be modified (or null if this url will not be used)
+	 * @throws JspException if an error occurs preparing the URL
+	 */
+	protected String url(String url) throws JspException {
+		if (url == null) {
+			return (url);
+		}
+
+		// Start with an unadorned URL as specified
+		StringBuffer src = new StringBuffer(url);
+
+		// Return the final result
+		return (src.toString());
+	}
+}
---
+++ core/src/main/java/net/jawr/web/taglib/jsf/AbstractImageTag.java
@@ -0,0 +1,216 @@
+/**
+ * Copyright 2009 Ibrahim Chaehoi
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of the License at
+ * 
+ * 	http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the
+ * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package net.jawr.web.taglib.jsf;
+
+import java.io.IOException;
+
+import javax.faces.component.UIOutput;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.jsp.JspException;
+
+import net.jawr.web.JawrConstant;
+import net.jawr.web.resource.ImageResourcesHandler;
+import net.jawr.web.resource.bundle.factory.util.PathNormalizer;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Abstract implementation of a facelets taglib component which will display images.  
+ * 
+ * @author Ibrahim Chaehoi
+ *
+ */
+public abstract class AbstractImageTag extends UIOutput {
+
+	/** The logger */
+	private static Logger logger = Logger.getLogger(AbstractImageTag.class);
+	
+	/* (non-Javadoc)
+	 * @see javax.faces.component.UIComponentBase#encodeBegin(javax.faces.context.FacesContext)
+	 */
+	public void encodeBegin(FacesContext context) throws IOException {
+		
+		// Initialize attributes
+		String src = (String)getAttributes().get("src"); 
+        
+        // src is mandatory
+        if(null == src)
+        	throw new IllegalStateException("The src attribute is mandatory for this Jawr tag. ");
+        
+        
+        render(context);
+        
+ 		super.encodeBegin(context);
+	}
+	
+	/**
+	 * Renders the image tag
+	 * @param context the faces context
+	 * @throws IOException if an IO exception occurs.
+	 */
+	protected abstract void render(FacesContext context) throws IOException ;
+
+	/**
+	 * Returns the attribute value associated to the name passed in parameter
+	 * @param attributeName the attribute name
+	 * @return the attribute value
+	 */
+	protected Object getAttribute(String attributeName){
+	
+		return getAttributes().get(attributeName);
+	}
+	
+	/**
+     * Prepares the style attributes for inclusion in the component's HTML
+     * tag.
+     *
+     * @return The prepared String for inclusion in the HTML tag.
+     * @throws JspException if invalid attributes are specified
+     */
+    protected String prepareStyles() {
+        StringBuffer styles = new StringBuffer();
+
+        prepareAttribute(styles, "id", getAttribute("styleId"));
+        prepareAttribute(styles, "style", getAttribute("style"));
+        prepareAttribute(styles, "class", getAttribute("styleClass"));
+        prepareAttribute(styles, "title", getAttribute("title"));
+        prepareAttribute(styles, "alt", getAttribute("alt"));
+        prepareInternationalization(styles);
+
+        return styles.toString();
+    }
+
+    /**
+     * Prepares the internationalization attributes, appending them to the the given
+     * StringBuffer.
+     *
+     * @param handlers The StringBuffer that output will be appended to.
+     * @since Struts 1.3.6
+     */
+    protected void prepareInternationalization(StringBuffer handlers) {
+        prepareAttribute(handlers, "lang", getAttribute("lang"));
+        prepareAttribute(handlers, "dir", getAttribute("dir"));
+    }
+
+    /**
+     * Prepares the event handlers for inclusion in the component's HTML tag.
+     *
+     * @return The prepared String for inclusion in the HTML tag.
+     */
+    protected String prepareEventHandlers() {
+        StringBuffer handlers = new StringBuffer();
+
+        prepareMouseEvents(handlers);
+        prepareKeyEvents(handlers);
+    
+        return handlers.toString();
+    }
+
+    /**
+     * Prepares the mouse event handlers, appending them to the the given
+     * StringBuffer.
+     *
+     * @param handlers The StringBuffer that output will be appended to.
+     */
+    protected void prepareMouseEvents(StringBuffer handlers) {
+        prepareAttribute(handlers, "onclick", getAttribute("onclick"));
+        prepareAttribute(handlers, "ondblclick", getAttribute("ondblclick"));
+        prepareAttribute(handlers, "onmouseover", getAttribute("onmouseover"));
+        prepareAttribute(handlers, "onmouseout", getAttribute("onmouseout"));
+        prepareAttribute(handlers, "onmousemove", getAttribute("onmousemove"));
+        prepareAttribute(handlers, "onmousedown", getAttribute("onmousedown"));
+        prepareAttribute(handlers, "onmouseup", getAttribute("onmouseup"));
+    }
+
+    /**
+     * Prepares the keyboard event handlers, appending them to the the given
+     * StringBuffer.
+     *
+     * @param handlers The StringBuffer that output will be appended to.
+     */
+    protected void prepareKeyEvents(StringBuffer handlers) {
+        prepareAttribute(handlers, "onkeydown", getAttribute("onkeydown"));
+        prepareAttribute(handlers, "onkeyup", getAttribute("onkeyup"));
+        prepareAttribute(handlers, "onkeypress", getAttribute("onkeypress"));
+    }
+
+    /**
+     * Prepares an attribute if the value is not null, appending it to the the
+     * given StringBuffer.
+     *
+     * @param handlers The StringBuffer that output will be appended to.
+     */
+    protected void prepareAttribute(StringBuffer handlers, String name,
+        Object value) {
+        if (value != null) {
+            handlers.append(" ");
+            handlers.append(name);
+            handlers.append("=\"");
+            handlers.append(value);
+            handlers.append("\"");
+        }
+    }
+    
+    /**
+     * Prepares an attribute if the value is not null, appending it to the the
+     * given StringBuffer.
+     *
+     * @param handlers The StringBuffer that output will be appended to.
+     */
+    protected void prepareAttribute(StringBuffer handlers, String name,
+        boolean value) {
+    
+    	handlers.append(" ");
+        handlers.append(name);
+        handlers.append("=\"");
+        handlers.append(value);
+        handlers.append("\"");
+    
+    }
+
+    /**
+	 * Prepare the image URL
+	 * @param context the faces context
+	 * @param results the result
+	 */
+	protected void prepareImageUrl(FacesContext context, StringBuffer results) {
+		
+		ImageResourcesHandler imgRsHandler = (ImageResourcesHandler) context.getExternalContext().getApplicationMap().get(JawrConstant.IMG_CONTEXT_ATTRIBUTE);
+		if(null == imgRsHandler)
+			throw new IllegalStateException("ImageResourceHandler not present in servlet context. Initialization of Jawr either failed or never occurred.");
+
+		String imgSrc = (String) getAttribute("src");
+		String newUrl = (String) imgRsHandler.getCacheUrl(imgSrc);
+
+        if(newUrl == null){
+        	newUrl = imgSrc;
+        	logger.debug("No mapping found for the image : "+imgSrc);
+        }
+        
+        String imageServletMapping = imgRsHandler.getJawrConfig().getServletMapping();
+		if("".equals(imageServletMapping)){
+			if(newUrl.startsWith("/")){
+				newUrl = newUrl.substring(1);
+			}
+		}else{
+			newUrl = PathNormalizer.joinDomainToPath(imageServletMapping, newUrl);
+		}
+        
+		HttpServletResponse response = ((HttpServletResponse)context.getExternalContext().getResponse());
+        prepareAttribute(results, "src", response.encodeURL(newUrl));
+	}
+
+	
+}
