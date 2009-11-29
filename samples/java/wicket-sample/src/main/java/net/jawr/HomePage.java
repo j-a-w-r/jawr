c01dfde8109408ac00cbd4f77c5428a4fad40763
Ibrahim Chaehoi <ichaehoi@gmail.com>
2009-11-29 16:26:56 +0000

    Add wicket sample application
    git-svn-id: https://svn.java.net/svn/jawr~svn/branches/3.x_development@495 4db09855-0bbe-6bc9-8408-d5225a1fabd9

---
+++ samples/java/wicket-sample/src/main/java/net/jawr/HomePage.java
@@ -0,0 +1,22 @@
+package net.jawr;
+
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.WebPage;
+
+/**
+ * Homepage
+ */
+public class HomePage extends WebPage {
+
+	private static final long serialVersionUID = 1L;
+
+    /**
+	 * Constructor that is invoked when page is invoked without a session.
+	 * 
+	 * @param parameters
+	 *            Page parameters
+	 */
+    public HomePage(final PageParameters parameters) {
+       
+    }
+}
